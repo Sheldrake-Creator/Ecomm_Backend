d14b7372502087e58d1259f0abeed8508b81b384
silas.pacheco <silasfpacheco@gmail.com>
2024-06-29 17:47:51 -0400

    reconfigured logging aspect

--- src/main/java/com/Config/LoggingAspect.java
+++ src/main/java/com/Config/LoggingAspect.java
@@ -14,22 +14,21 @@ public class LoggingAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Before("execution(* com.*(..))")
+    @Before("execution(* com..*(..))")
     public void logMethodEntry(JoinPoint joinPoint) {
         if (logger.isDebugEnabled()) {
-            String className = joinPoint.getSignature().getDeclaringTypeName();
+            String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
             String methodName = joinPoint.getSignature().getName();
             logger.debug("Entering method: {}.{}", className, methodName);
         }
     }
 
-    @After("execution(* com.*(..))")
+    @After("execution(* com..*(..))")
     public void logMethodExit(JoinPoint joinPoint) {
         if (logger.isDebugEnabled()) {
-            String className = joinPoint.getSignature().getDeclaringTypeName();
+            String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
             String methodName = joinPoint.getSignature().getName();
             logger.debug("Exiting method: {}.{}", className, methodName);
         }
     }
 }
-
